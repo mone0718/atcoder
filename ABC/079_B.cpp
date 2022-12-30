b193db94ffeca8912f6ac1847185fe5c22568e51
mone0718 <monefujita.eb@keio.jp>
2022-12-30 17:13:15 +0900

    数値型(APG4b 3.01)

--- ABC/079_B.cpp
+++ ABC/079_B.cpp
@@ -5,6 +5,8 @@ int main () {
     int n;
     cin >> n;
 
+    //int64_t 扱える値の範囲:-2^63 ~ 2^63-1 メモリ量:8byte (64bit)
+    //int 扱える値の範囲:-2^31 ~ 2^31-1 メモリ量:4byte (32bit)
     vector<int64_t> a(n+1);
     a.at(0) = 2, a.at(1) = 1;
      for (int i = 2; i < n+1; i++){
